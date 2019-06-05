ddcd97080fd5d841c5ef4e0add45408a200f59aa
MichelML <michmoreau.l@gmail.com>
2019-06-04 23:42:24 -0400

    revert to if + brackets

--- ttf.js
+++ ttf.js
@@ -2,33 +2,37 @@ const im = require("gm").subClass({ imageMagick: true });
 const jimp = require("jimp");
 
 module.exports = async (msg, x, y, path, size, HexColor) => {
-  (!x || !y)
-    && throw new Error("[ttfFont][ALERT] Error: X or Z is not specified !");
-  (!path || path === undefined)
-    && throw new Error(
-        "[ttfFont][ALERT] Error: Path is not specified or is undefined !"
-        );
-  (!size || size === undefined)
-    && throw new Error(
-        "[ttfFont][ALERT] Error: Size is not specified or is undefined !"
-        );
-  (!HexColor || HexColor === undefined)
-    && throw new Error(
-        "[ttfFont][ALERT] Error: HexColor is not specified or is undefined !"
-        );
+	if (!x || !y) {
+		throw new Error("[ttfFont][ALERT] Error: X or Z is not specified !");
+	}
+	if (!path || path === undefined) {
+		throw new Error(
+				"[ttfFont][ALERT] Error: Path is not specified or is undefined !"
+				);
+	}
+	if (!size || size === undefined) {
+		throw new Error(
+				"[ttfFont][ALERT] Error: Size is not specified or is undefined !"
+				);
+	}
+	if (!HexColor || HexColor === undefined) {
+		throw new Error(
+				"[ttfFont][ALERT] Error: HexColor is not specified or is undefined !"
+				);
+	}
 
-  return new Promise(async (resolve, reject) => {
-    let img = im(x, y).command("convert"); //width, height
-    img.font(path, size); //path, Font size
-    img.out("-fill").out(HexColor); //HexaColor important!
-    img.out("-background").out("transparent");
-    img.out("-gravity").out("west");
-    img.out(`caption:${msg}`); //Text
-    img.setFormat("png").toBuffer(async function(err, buffer) {
-      if (err) throw reject(`[ttfFont][ALERT] ${err} !`);
-      await resolve(buffer);
-    });
-  }).then(async buffer => {
-    return await jimp.read(buffer);
-  });
+	return new Promise(async (resolve, reject) => {
+		let img = im(x, y).command("convert"); //width, height
+		img.font(path, size); //path, Font size
+		img.out("-fill").out(HexColor); //HexaColor important!
+		img.out("-background").out("transparent");
+		img.out("-gravity").out("west");
+		img.out(`caption:${msg}`); //Text
+		img.setFormat("png").toBuffer(async function(err, buffer) {
+			if (err) throw reject(`[ttfFont][ALERT] ${err} !`);
+			await resolve(buffer);
+		});
+	}).then(async buffer => {
+		return await jimp.read(buffer);
+	});
 };
