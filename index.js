d98bee3104e56987a7cee4160c714695b4ddf374
MichelML <michmoreau.l@gmail.com>
2019-06-05 00:27:12 -0400

    log every options

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const path = require("path");
 const fontsMap = require("./fonts-map");
 const ttf = require("./ttf");
 const argv = require("yargs")
-  .option("resize", {
-    alias: "r"
-  })
-  .option("bgColor", {
-    alias: "b",
-    default: "black"
-  })
-  .option("color", {
-    alias: "c",
-    default: "white"
-  })
-  .option("message", {
-    alias: "m"
-  })
-  .option("fontsize", {
-    alias: "s",
-    default: 200
-  })
-  .option("fontfamily", {
-    alias: "f",
-    default: "montserrat"
-  }).argv;
+.option("resize", {
+	alias: "r"
+})
+.option("bgColor", {
+	alias: "b",
+	default: "black"
+})
+.option("color", {
+	alias: "c",
+	default: "white"
+})
+.option("message", {
+	alias: "m"
+})
+.option("fontsize", {
+	alias: "s",
+	default: 200
+})
+.option("fontfamily", {
+	alias: "f",
+	default: "montserrat"
+}).argv;
 
 const imageSize = 2160;
 const message = argv.message;
@@ -38,48 +38,55 @@ const marginX = (imageSize - width) / 2;
 const marginY = (imageSize - height) / 2;
 
 const wob = async ({
-  message,
-  width,
-  height,
-  marginX,
-  marginY,
-  resize,
-  bgColor
+	message,
+	width,
+	height,
+	marginX,
+	marginY,
+	resize,
+	bgColor
 }) => {
-  const image = await new Jimp(2160, 2160, bgColor);
-  const text = await ttf(
-    message,
-    width,
-    height,
-    path.join(__dirname, "fonts", fontsMap[argv.fontfamily]),
-    fontSize,
-    argv.color
-  );
-  image.composite(text, marginX, marginY);
-  const file = message.replace(/[^a-z0-9+]+/gi, "") + ".jpg";
-  await image.writeAsync(message.replace(/[^a-z0-9+]+/gi, "") + ".jpg");
-  if (resize) {
-    await sharp(file)
-      .resize(resize)
-      .toFile(file.replace(".jpg", resize.toString() + ".jpg"));
-  }
+	const image = await new Jimp(2160, 2160, bgColor);
+	const text = await ttf(
+			message,
+			width,
+			height,
+			path.join(__dirname, "fonts", fontsMap[argv.fontfamily]),
+			fontSize,
+			argv.color
+			);
+	image.composite(text, marginX, marginY);
+	const file = message.replace(/[^a-z0-9+]+/gi, "") + ".jpg";
+	await image.writeAsync(message.replace(/[^a-z0-9+]+/gi, "") + ".jpg");
+	if (resize) {
+		await sharp(file)
+			.resize(resize)
+			.toFile(file.replace(".jpg", resize.toString() + ".jpg"));
+	}
 };
 
 (async () => {
-  if (!argv.message) {
-    console.log("No messsage provided.");
-    process.exit();
-  }
+	if (!argv.message) {
+		console.log("No messsage provided.");
+		process.exit();
+	}
 
-  console.log(`Using font: ${argv.fontfamily}`);
+	console.log(`
+			Message: ${argv.message}
+			Font color: ${argv.color}
+			Font size: ${argv.fontsize}
+			Font family: ${argv.fontfamily}
+			Background color: ${argv.bgColor}
+			Resize: ${argv.resize || "none"}
+			`);
 
-  await wob({
-    message: argv.message,
-    width,
-    height,
-    marginX,
-    marginY,
-    resize: argv.resize && parseInt(argv.resize, 10),
-    bgColor: argv.bgColor
-  });
+	await wob({
+		message: argv.message,
+		width,
+		height,
+		marginX,
+		marginY,
+		resize: argv.resize && parseInt(argv.resize, 10),
+		bgColor: argv.bgColor
+	});
 })();
